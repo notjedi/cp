192dc7110294d1eebd98fef86ec405ce97c428f4
Krithic Kumar <krithickumarub@protonmail.com>
2021-12-05 19:22:05 +0530

    change method name and gitignore

--- aoc/2021/day4.py
+++ aoc/2021/day4.py
@@ -1,15 +1,13 @@
 def checkBoard(board):
 
-    # check columns
-    for i in range(5):
-        # print(board[i::5])
-        if all(board[i::5]):
-            return True
-
     # check rows
     for i in range(0, 25, 5):
-        # print(board[i:i+5])
         if all(board[i: i+5]):
+            return True
+
+    # check columns
+    for i in range(5):
+        if all(board[i::5]):
             return True
 
     return False
@@ -23,10 +21,10 @@ def sumBoard(board, mask):
 
     return boardSum
 
-def part1(nums, boards):
+def solve(nums, boards):
     mask = [[0 for _ in range(25)] for _ in range(len(boards))]
-    ans1, ans2 = None, None
     boardWin = [0 for _ in range(len(boards))]
+    part1, part2= None, None
 
     for num in nums:
         for i, board in enumerate(boards):
@@ -35,19 +33,18 @@ def part1(nums, boards):
             mask[i][board.index(num)] = 1
             if (checkBoard(mask[i])):
                 boardSum = sumBoard(board, mask[i])
-                if (ans1 is None):
-                    ans1 = num * boardSum
+                if (part1 is None):
+                    part1 = num * boardSum
                 if (boardWin[i] == 0):
                     boardWin[i] = 1
-                    ans2 = num * boardSum
+                    part2 = num * boardSum
 
-    print(ans1)
-    print(ans2)
+    print(part1)
+    print(part2)
 
 
 if __name__ == '__main__':
     file = open("aoc/2021/inputs/day4", "r")
-    # file = open("aoc/2021/inputs/samp", "r")
     nums = list(map(int, file.readline().strip().split(',')))
     boards = []
     for board in file.read().strip().split('\n\n'):
@@ -59,4 +56,4 @@ if __name__ == '__main__':
         assert len(X) == 25
         boards.append(X)
 
-    part1(nums, boards)
+    solve(nums, boards)
