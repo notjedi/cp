64a21247a67a7c3a93a4932bc7072a998470ae3f
Krithic Kumar <krithickumarub@protonmail.com>
2021-12-19 08:17:48 +0530

    add day 9 solutions

---
+++ aoc/2021/day9.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+def expand(i, j):
+
+    if i < 0 or i >= rows:
+        return 0
+    elif j < 0 or j >= cols:
+        return 0
+    elif lines[i][j] == 9:
+        return 0
+
+    count = 0
+    pts[i][j] = True
+    for dx in [i+1, i-1]:
+        if dx >= 0 and dx < rows and (lines[i][j] < lines[dx][j]) and lines[dx][j] != 9 and pts[dx][j] == 0:
+            pts[dx][j] = True
+            count += 1
+            count += expand(dx, j)
+
+    for dy in [j+1, j-1]:
+        if dy >= 0 and dy < cols and (lines[i][j] < lines[i][dy]) and lines[i][dy] != 9 and pts[i][dy] == 0:
+            pts[i][dy] = True
+            count += 1
+            count += expand(i, dy)
+
+    return count
+
+
+def check_adj(lines, i, j):
+    ok = True
+    for dx in [i+1, i-1]:
+        if dx >= 0 and dx < rows and lines[dx][j] <= lines[i][j]:
+            ok = False
+
+    for dy in [j+1, j-1]:
+        if dy >= 0 and dy < cols and lines[i][dy] <= lines[i][j]:
+            ok = False
+
+    return ok
+
+
+def solve():
+    part1 = 0
+    part2 = 1
+    lowPts = []
+
+    for i in range(len(lines)):
+        for j in range(len(lines[0])):
+            if check_adj(lines, i, j):
+                lowPts.append((i, j))
+                part1 += (1 + lines[i][j])
+
+
+    largest = []
+    for point in lowPts:
+        count = expand(*point)
+        largest.append(count + 1)
+
+    largest = sorted(largest, reverse=True)
+    for i in range(3):
+        part2 *= largest[i]
+
+    print(part1)
+    print(part2)
+
+
+if __name__ == "__main__":
+    file = open("aoc/2021/inputs/day9", "r")
+    lines = [list(map(int, list(x))) for x in file.read().strip().split('\n')]
+    pts = np.zeros((len(lines), len(lines[0])), dtype=np.int8)
+    rows, cols = len(lines), len(lines[0])
+    solve()
